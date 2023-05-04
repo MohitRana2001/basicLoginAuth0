b0e18439ec20bb64a990987e9fde8bce2f838dc7
MOHIT RANA <awesomemohit20@gmail.com>
2023-05-04 17:45:20 +0530

    trying diff things

--- profile.js
+++ profile.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const { requiresAuth } = require('express-openid-connect');
-
+const Usermodel = require('./models/User');
 const router = express.Router();
 
 router.get('/', requiresAuth(), (req, res) => {
+  // const user = new Usermodel({
+  //   email: req.oidc.user.email,
+  //   password: req.oidc.user.name
+  // });
+  // user.save();
+  console.log(req.oidc.user);
   res.render('profile', { user: req.oidc.user });
 });
 
